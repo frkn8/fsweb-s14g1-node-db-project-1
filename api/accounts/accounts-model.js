12109190bcb3059e74910417260aced26b75b7e8
frkn8 <103068163+frkn8@users.noreply.github.com>
2023-06-06 23:24:31 +0300

    tamamdı...

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,27 +1,90 @@
-const getAll = () => {
-  // KODLAR BURAYA
-}
+const router = require("express").Router();
+const accountsModel = require("./accounts-model");
+const mw = require("./accounts-middleware");
 
-const getById = id => {
+router.get("/", async (req, res, next) => {
   // KODLAR BURAYA
-}
+  try {
+    const { limit, sortby, sortdir } = req.query;
+    let query = accountsModel.getAll();
 
-const create = account => {
+    if (limit) {
+      query = query.limit(parseInt(limit));
+    }
+
+    if (sortby && sortdir) {
+      query = query.orderBy(sortby, sortdir);
+    }
+
+    const allAccounts = await query;
+    res.json(allAccounts);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", mw.checkAccountId, (req, res, next) => {
   // KODLAR BURAYA
-}
+  try {
+    res.json(req.existAccount);
+  } catch (error) {
+    next(error);
+  }
+});
 
-const updateById = (id, account) => {
+router.post(
+  "/",
+  mw.checkAccountPayload,
+  mw.checkAccountNameUnique,
+  async (req, res, next) => {
+    // KODLAR BURAYA
+    try {
+      const insertedRecord = await accountsModel.create({
+        name: req.body.name,
+        budget: req.body.budget,
+      });
+      res.status(201).json(insertedRecord);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.put(
+  "/:id",
+  mw.checkAccountId,
+  mw.checkAccountPayload,
+  async (req, res, next) => {
+    // KODLAR BURAYA
+    try {
+      const updatedRecord = await accountsModel.updateById(req.params.id, {
+        name: req.body.name,
+        budget: req.body.budget,
+      });
+      res.json(updatedRecord);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.delete("/:id", mw.checkAccountId, async (req, res, next) => {
   // KODLAR BURAYA
-}
+  try {
+    await accountsModel.deleteById(req.params.id);
+    res.json(req.existAccount);
+  } catch (error) {
+    next(error);
+  }
+});
 
-const deleteById = id => {
+router.use((err, req, res, next) => {
+  // eslint-disable-line
   // KODLAR BURAYA
-}
+  res.status(err.status || 500).json({
+    customMessage: "Global handler tarafında hata alındı",
+    message: err.message,
+  });
+});
 
-module.exports = {
-  getAll,
-  getById,
-  create,
-  updateById,
-  deleteById,
-}
+module.exports = router;
